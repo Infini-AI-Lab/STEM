1ae17f849d3958f9df1e5c289a2d4f2b08abfeb4
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-14 23:09:57 -0500

    added qwen and olmo support

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,3 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-223133.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-warmup-223133.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-230926.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-230926.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-warmup-230926.yaml
