1129e7c9927a1ed2bd75f9e6dcaa5cf7f6c62b3d
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-15 19:16:14 -0500

    setup lm eval tasks

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-151829.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-151829.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-191543.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-191543.yaml
