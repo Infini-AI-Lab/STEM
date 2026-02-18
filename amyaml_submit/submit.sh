6e347d64dd7124050725086fdc3dd7a3d8c6ecd4
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-17 20:23:21 -0500

    submit 2 nodes

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-152822.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-152822.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-202300.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-202300.yaml
