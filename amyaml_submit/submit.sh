0bf9e35ffe0d2dec97e095eb1771cdd9d485c789
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-18 20:18:17 -0500

    update config format

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-192551.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-192551.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-201741.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-201741.yaml
