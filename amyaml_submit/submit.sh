6003515fe29159fe8407e8b0782389b6603dbde2
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-19 18:34:18 -0500

    test multinode

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,4 +1,1 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-182804.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-225952.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-182804.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-w2-225952.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-183401.yaml
