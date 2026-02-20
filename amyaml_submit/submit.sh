614e90bf6e974e689156122ae9d6c4324c55cee1
99versatile <ehong@andrew.cmu.edu>
2026-02-19 19:06:09 -0500

    Update scale up experiments

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,4 +1,4 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-full-adafine-205536.yaml
-# kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-full-adafine-freeze-205536.yaml
-# kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-full-lorafine-205536.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-fullfine-205536.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-full-adafine-190443.yaml
+# kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-full-adafine-freeze-190443.yaml
+# kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-full-lorafine-190443.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-fullfine-190443.yaml
