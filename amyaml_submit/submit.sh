1cf135254c7fd2375265cce4f707efd7b02df8c5
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-23 19:42:19 -0500

    smaller subset

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-dag-193438.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-iir-193438.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-dag-194144.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-iir-194144.yaml
