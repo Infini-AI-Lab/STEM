71588606c97dd92179779da4cd08d4cbc2e0f019
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-23 20:38:02 -0500

    launch midtrain on stem_iir and stem_dag

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-dag-194144.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-iir-194144.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-dag-203723.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-iir-203723.yaml
