9b771f5e5b71a54bdd4209a39b719c0f0a284cf9
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-24 19:57:27 -0500

    relaunch

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,3 +1,1 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-dag-153928.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-iir-153928.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-stem-153928.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-dag-195700.yaml
