a33ca5aadc9fd0289a98496bba83b8d4f7e0f9f9
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-02-25 20:12:47 -0500

    math finetuning

---
+++ amyaml_submit/scripts/lm1b-midmath-base.sh
@@ -0,0 +1,46 @@
+export PYTHONPATH=/code-fsx/beidchen-sandbox/STEM:$PYTHONPATH
+
+set -x
+
+project_name="stem"
+experiment_name="lm1b-midmath-base-10B"
+NNODES=2
+
+export TORCHINDUCTOR_CACHE_DIR=/scratch/scratch/beidchen/torchinductor_cache/${HOSTNAME} 
+WANDB_DIR=/scratch/scratch/beidchen/projects/stem_wandb 
+export WANDB_API_KEY="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"
+export WANDB_DIR
+export WANDB_MODE=offline
+
+echo "$HOSTNAME $(hostname -I)"
+echo "$HOSTNAME $(hostname -I | awk '{print $2}')"
+
+if [ "${HOSTNAME##*-}" -eq 0 ]; then
+    export WANDB_MODE=online
+else
+    export WANDB_MODE=offline
+fi
+
+
+NODE_RANK=${HOSTNAME##*-}
+echo "NODE_RANK: $NODE_RANK"
+echo "WANDB_MODE: $WANDB_MODE"
+
+
+python3 setup/aws_prepare_hf_dataset.py \
+    --local_dir /dev/shm/dolmino_math-mix \
+    --out_dir /dev/shm/dolmino-mix_math_shuffled \
+    --dataset dolmino-mix-math \
+    --num_nodes ${NNODES} \
+    --node_rank ${NODE_RANK} \
+    --nchunks 8 
+
+rm -rf /dev/shm/data
+
+torchrun --nproc-per-node=8 --nnodes=${NNODES} -m apps.main.train \
+    config=apps/main/configs/llama3_1B_midmath.yaml \
+    data.root_dir=/dev/shm \
+    dump_dir=/checkpoints-fsx/beidchen-sandbox/STEM/logs/${experiment_name} \
+    checkpoint.init_ckpt_path=/checkpoints-fsx/beidchen-sandbox/stem/Llama-3.2-1B/distcp/ \
+    data.tokenizer.path=/checkpoints-fsx/beidchen-sandbox/stem/Llama-3.2-1B/original/tokenizer.model \
+    logging.wandb.name=${experiment_name} 
--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,1 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-192714.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midfine-base-195921.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-lm1b-midmath-base-195921.yaml
