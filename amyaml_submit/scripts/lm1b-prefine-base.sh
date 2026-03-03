e598bd5867ee83c290919f65f810804024dd4f84
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-03-03 18:55:42 -0500

    relaunch

--- amyaml_submit/scripts/lm1b-prefine-base.sh
+++ amyaml_submit/scripts/lm1b-prefine-base.sh
@@ -4,7 +4,7 @@ set -x
 
 project_name="stem"
 experiment_name="lm1b-prefine-base-100B"
-NNODES=2
+NNODES=4
 
 export TORCHINDUCTOR_CACHE_DIR=/scratch/scratch/beidchen/torchinductor_cache/${HOSTNAME} 
 WANDB_DIR=/scratch/scratch/beidchen/projects/stem_wandb 
