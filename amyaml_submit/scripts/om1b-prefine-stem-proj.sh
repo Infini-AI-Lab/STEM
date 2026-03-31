60e4d4c1817d91bf2560c7f7d66df7be8b5c93ee
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-03-31 18:46:17 -0400

    minor update

--- amyaml_submit/scripts/om1b-prefine-stem-proj.sh
+++ amyaml_submit/scripts/om1b-prefine-stem-proj.sh
@@ -55,7 +55,7 @@ torchrun --nproc-per-node=8 --nnodes=${NNODES} -m apps.main.stem_projection_warm
     data.tokenizer.path=/checkpoints-fsx/beidchen-sandbox/stem/olmo2-1b-stage1-token1T/ \
     logging.wandb.name=stem_projection_warmup_olmo2_1B \
     stem_layers=[1,2,3,4] \
-    steps=2500
+    steps=5000
 
 
 # confirm the directory exists
@@ -66,7 +66,7 @@ fi
 
 python3 -m apps.main.prepare_reparam_init_checkpoint  \
     --base-init-ckpt-path /checkpoints-fsx/beidchen-sandbox/stem/olmo2-1b-stage1-token1T   \
-    --warmup-ckpt-path /dev/shm/logs/stem_projection_warmup_olmo2_1B/checkpoints/0000002500  \
+    --warmup-ckpt-path /dev/shm/logs/stem_projection_warmup_olmo2_1B/checkpoints/0000005000  \
     --output-dir /dev/shm/olmo2-1b-reparam-init \
     --stem-parallel-size 2 
 
