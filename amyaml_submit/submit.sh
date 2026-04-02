0b23add9950bf0ab8f71000c44528edafb543337
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-04-01 22:23:35 -0400

    relaunch

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,1 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-base-183742.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-stem-distill-183742.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-stem-cmptok-222256.yaml
