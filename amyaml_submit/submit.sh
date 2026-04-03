196794e46796a662f9b968a0094831fed9e7f1c3
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-04-03 14:38:36 -0400

    relaunch

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-basernd-172508.yaml
+# kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-basernd-172508.yaml
 kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-stemf-172508.yaml
