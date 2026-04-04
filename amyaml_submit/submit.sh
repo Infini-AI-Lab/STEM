5c51d511f92d3fef0f874554fbcc22ee98ce119a
rsadhukh <rsadhukh@andrew.cmu.edu>
2026-04-04 11:37:01 -0400

    relaunch

--- amyaml_submit/submit.sh
+++ amyaml_submit/submit.sh
@@ -1,2 +1,2 @@
-kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-stem-distill-184603.yaml
-kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prewarm-stem-distill-184603.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prefine-stem-distill-113558.yaml
+kubectl apply -f amyaml_submit/scripts/beidchen-om1b-prewarm-stem-distill-113558.yaml
