d06937064fb97c905137f492eb33d3060c8d02da
Ranajoy Sadhukhan <rsadhukh@catalyst-fleet1.cs.cmu.edu>
2026-04-12 16:20:58 -0400

    relaunch

--- amyaml_submit/scripts/om1b-midfine-stem-dag.sh
+++ amyaml_submit/scripts/om1b-midfine-stem-dag.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=/code-fsx/beidchen-sandbox/STEM:$PYTHONPATH
 set -x
 
 project_name="stem"
-experiment_name="olmo2-1b-stem-dag-4T-midfine100B"
+experiment_name="olmo2-1b-stem-dag-4T-midfine100B-2"
 NNODES=4
 
 export TORCHINDUCTOR_CACHE_DIR=/scratch/scratch/beidchen/torchinductor_cache/${HOSTNAME} 
